bea73dec5069f728a5a9f828cdbd73f84d766653
smenon18 <smenon18@gmail.com>
2016-03-12 18:02:53 -0500

    added api to installed_apps, added necessary imports to api.views, created api.serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
--- djangodev/settings.py
+++ djangodev/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'pages',
+    'api',
 ]
 
 MIDDLEWARE_CLASSES = [
