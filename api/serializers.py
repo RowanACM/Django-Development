a89039fd6e3a5a2ddbde54edab8bc6565b885b9c
Chris Pryer <christophpryer@gmail.com>
2016-03-20 20:40:09 -0400

    Post-Merge issue resolution* update

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,12 @@
 from rest_framework import serializers
 
-<<<<<<< HEAD
-=======
 
->>>>>>> f986dd158f6425084296b38923cdb173b25f7e64
 from models import Member
 from rest_framework import serializers
 
 
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
-<<<<<<< HEAD
     class Meta:
         model = Member
         fields = ('id', 'first_name', 'last_name', 'meeting_attend')
 
-=======
-	class Meta:
-            	model = Member
-		fields = ('id', 'first_name', 'last_name', 'meeting_attend')
->>>>>>> f986dd158f6425084296b38923cdb173b25f7e64
