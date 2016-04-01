9c624a40d237a420e905bc5ea9fb9bb2e1f8cce0
RichCerone <rcerone@live.com>
2016-04-01 15:57:36 -0400

    Fixed a bug in urls.py under api. Someone forgot to reference the method function under the class MemberViewSet. For example, it should be MemberViewSet.list_members.

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^member/view_all', views.list_members),
-    url(r'^member/post', views.post_member),
+    url(r'^member/view_all', views.MemberViewSet.list_members),
+    url(r'^member/post', views.MemberViewSet.post_member),
 ]
