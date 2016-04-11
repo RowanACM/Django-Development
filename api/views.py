9b851c6a4da39f6b3bbfdfc6c72356b8a8eb1932
Chris <christophpryer@gmail.com>
2016-04-11 19:24:27 -0300

    added a status import (response left incase we need it?)

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework import status
 
 from models import Member, Meetings
 
