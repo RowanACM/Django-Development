ba05ec5f3a5c20e609c7ab0d2406132b19e6865a
Chris <christophpryer@gmail.com>
2016-04-14 14:22:44 -0300

    removed user import

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import admin, user
+from django.contrib.auth import admin
 
 from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
